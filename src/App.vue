49e471b140a8c1e689ad8603caafc06dacbcf518
syan0143 <syan0143@student.monash.edu>
2024-08-27 22:08:01 +1000

    third

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,17 @@ import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
+  <div class="main-container">
   <header>
     <BHeader />
   </header>
 
-  <main>
-    <LibraryRegistrationForm />
+  <main class="main-box">
+    <!-- <LibraryRegistrationForm /> -->
     <!-- <JSONLab /> -->
+     <router-view></router-view>
   </main>
+  </div>
 </template>
 
 <style scoped>
@@ -42,4 +45,42 @@ import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
     flex-wrap: wrap;
   }
 } */
+
+.container {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  max-width: 80vw;
+  margin: 0 auto;
+  padding: 20px;
+  /* background-color: #e0bfbf; */
+  border-radius: 10px;
+}
+
+/* Class selectors */
+.form {
+  text-align: center;
+  margin-top: 50px;
+}
+
+/* ID selectors */
+#username:focus,
+#password:focus,
+#isAustralian:focus,
+.card {
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.card-header {
+  background-color: #275fda;
+  color: white;
+  padding: 10px;
+  border-radius: 10px 10px 0 0;
+}
+.list-group-item {
+  padding: 10px;
+}
+
+.about{
+  text-align: center;
+}
 </style>
